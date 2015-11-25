32e10543dfcbdd07eb11cad99328da53fbba72c5
lizababy <lizababy88@gmail.com>
2015-11-25 13:22:36 -0500

    Initial Commit

---
+++ Kitchen ManagerTests/Kitchen_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kitchen_ManagerTests.swift
+//  Kitchen ManagerTests
+//
+//  Created by Liza Linto on 11/25/15.
+//  Copyright © 2015 Liza Linto. All rights reserved.
+//
+
+import XCTest
+@testable import Kitchen_Manager
+
+class Kitchen_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
